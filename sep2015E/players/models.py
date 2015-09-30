29077fff7052c92dd76c8e2355fbaacd2decbe7c
adethise <arnaud.dethise@gmail.com>
2015-09-30 16:21:28 +0200

    added players.User.phone

--- sep2015E/players/models.py
+++ sep2015E/players/models.py
@@ -8,7 +8,9 @@ class User(models.Model):
     country = models.CharField(max_length=64)
     zipcode = models.IntegerField(default=0)
     email = models.CharField(max_length=128)
+    phone = models.CharField(max_length=32)
     level = models.CharField(max_length=8)
+
     def __str__(self):              # __unicode__ on Python 2
         return self.lastname + " " + self.firstname
     
@@ -16,5 +18,6 @@ class Pair(models.Model):
     player1 = models.ForeignKey(User, related_name='player1')
     player2 = models.ForeignKey(User, related_name='player2')
     average = models.IntegerField(default=0)
+
     def __str__(self):              # __unicode__ on Python 2
         return self.player1.lastname + " " + self.player2.lastname
