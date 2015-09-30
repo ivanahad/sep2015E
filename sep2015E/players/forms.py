6012d21e4feac119c7d28e8eacff388b41187afc
adepotter <Aurian@MacBook-Pro-de-Aurian.local>
2015-09-30 16:41:35 +0200

    mini update

--- sep2015E/players/forms.py
+++ sep2015E/players/forms.py
@@ -4,4 +4,4 @@ from players.models import User
 class RegisterForm(forms.ModelForm):
 	class Meta:
 		model = User
-		fields = ('firstname','lastname','address','city','country','zipcode','email','level',)
+		fields = '__all__'
