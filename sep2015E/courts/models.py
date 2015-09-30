e01c4c70d4315e70dc9c8a0d8f416b576f80913c
adethise <arnaud.dethise@gmail.com>
2015-09-30 17:06:51 +0200

    created models for app courts

--- sep2015E/courts/models.py
+++ sep2015E/courts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Court(models.Model):
+    """This class defines a tennis court.
+    It also includes informations such as how the court can be used,
+    what his type is and who is the owner."""
+    owner = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=64)
+    zipcode = models.IntegerField(default=0)
+    email = models.EmailField()
+    phone = models.CharField(max_length=32)
+    ground = models.CharField(max_length=128)
+    cover = models.BooleanField(default=False)
+    image = models.FileField(null=True)
+    comment_access = models.TextField(default="")
+    comment_desiderata = models.TextField(default="")
