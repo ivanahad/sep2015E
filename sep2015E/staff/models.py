e269c9d727008738ba888fb5caefa5582b5ef5f9
edd19 <eddyndizera@gmail.com>
2015-10-06 18:28:11 +0200

    Ajout model staff

--- sep2015E/staff/models.py
+++ sep2015E/staff/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Staff(models.Model):
+    """This class contains the record on a staff member."""
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    zipcode = models.IntegerField(default=0)
+    email = models.EmailField(max_length=128)
+    phone = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.lastname + " " + self.firstname
