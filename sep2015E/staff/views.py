2a1e7c7a42b13b77883ccc991c528b61d1d6232b
edd19 <eddyndizera@gmail.com>
2015-10-08 13:53:47 +0200

    merge

--- sep2015E/staff/views.py
+++ sep2015E/staff/views.py
@@ -6,4 +6,5 @@ name = "Eric Duvoie" #replace by a call to database
 
 def home(request):
     """Home page for staff members."""
-    return render(request, 'staff/home.html')
+    info = {'date': datetime.now(), 'name': name}
+    return render(request, 'staff/home.html', info)
