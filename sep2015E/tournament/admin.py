575713c506e621b580028675191c6643002df7b6
Siceron <ludovicfastre@yahoo.fr>
2015-10-08 17:44:09 +0200

    Admin close tournament

--- sep2015E/tournament/admin.py
+++ sep2015E/tournament/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from .models import Tournament, TournamentParticipant, Pool, PoolParticipant, Match, PoolMatch, TournamentNode
 
-admin.site.register(Tournament)
+def close_action(modeladmin, request, queryset):
+    queryset.update(is_open=False)
+close_action.short_description = "Close tournament"
+
+def open_action(modeladmin, request, queryset):
+    queryset.update(is_open=True)
+open_action.short_description = "Open tournament"
+
+class MyAdmin(admin.ModelAdmin):
+    list_display=('name', 'is_open')
+    actions = [close_action, open_action]
+
+admin.site.register(Tournament, MyAdmin)
 admin.site.register(TournamentParticipant)
 admin.site.register(Pool)
 admin.site.register(PoolParticipant)
