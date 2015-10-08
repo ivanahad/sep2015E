62ef9217f1f626758f90c6ed236c7ffeb9d94dcd
Siceron <ludovicfastre@yahoo.fr>
2015-10-08 12:30:44 +0200

    Add tables in admin

--- sep2015E/tournament/admin.py
+++ sep2015E/tournament/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tournament, TournamentParticipant, Pool, PoolParticipant, Match, PoolMatch, TournamentNode
+
+admin.site.register(Tournament)
+admin.site.register(TournamentParticipant)
+admin.site.register(Pool)
+admin.site.register(PoolParticipant)
+admin.site.register(Match)
+admin.site.register(PoolMatch)
+admin.site.register(TournamentNode)
