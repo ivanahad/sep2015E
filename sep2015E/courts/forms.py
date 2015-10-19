ade748a7f6cddce4f344d13309b4f0dd1f3ec2ad
Arnaud Dethise <arnauddethise@gmail.com>
2015-10-19 23:16:56 +0200

    minor changes to court registration form

--- sep2015E/courts/forms.py
+++ sep2015E/courts/forms.py
@@ -2,10 +2,15 @@ from django import forms
 from courts.models import Court
 
 class RegisterForm(forms.ModelForm):
-
     class Meta:
         model = Court
         fields = '__all__'
+        exclude = ['available']
+        widgets = {
+                'comment_access': forms.Textarea(attrs={'cols': 40, 'rows':3}),
+                'comment_desiderata': forms.Textarea(attrs={'cols': 40, 'rows':3})
+            }
+                
 
 class OwnerCourtsForm(forms.Form):
 	owner = forms.CharField(label='Owner')
