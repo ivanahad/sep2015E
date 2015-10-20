090a15c848068d063947b5828eb364ec9edf5c8f
Arnaud Dethise <arnauddethise@gmail.com>
2015-10-20 23:29:47 +0200

    removed old close tournament box

--- sep2015E/staff/views.py
+++ sep2015E/staff/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from datetime import datetime
 from staff.models import Messages
 from staff.forms import MessageForm
-from tournament.forms import OpenTournamentChoiceForm
 from courts.models import Court
 
 
@@ -21,13 +20,9 @@ def home(request):
                     title = form_msg.cleaned_data['title'], \
                     message = form_msg.cleaned_data['message']).save()
 
-        form_trn = OpenTournamentChoiceForm(request.POST, prefix="trn")
-        if form_trn.is_valid():
-            form_trn.cleaned_data['tournament'].close_registrations()
 
 
     form_msg = MessageForm(prefix="msg")
-    form_trn = OpenTournamentChoiceForm(prefix="trn")
     messages = Messages.objects.all() #replace by a call destined to the current staff member
 
     return render(request, 'staff/home.html', {\
@@ -35,7 +30,6 @@ def home(request):
             'date':datetime.now(), \
             'messages':messages, \
             'form_msg': form_msg,
-            'form_trn': form_trn,\
             })
 
 def courts(request):
