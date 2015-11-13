8a663affdd028bca76a95214c2d40a1a1052afcb
Siceron <ludovicfastre@yahoo.fr>
2015-11-13 21:27:47 +0100

    Navbar staff tournament detail bug

--- sep2015E/tournament/urls.py
+++ sep2015E/tournament/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('tournament.views',
     url(r'^all$', 'all'),
     url(r'^allUser$', 'allUser'),
     url(r'^detail/(?P<id_>[0-9]+)$', 'tournament'),
+    url(r'^detailStaff/(?P<id_>[0-9]+)$', 'tournamentStaff'),
     url(r'^(?P<id_tournament>\d+)/pool/(?P<id_pool>\d+)$', 'pool'),
     url(r'^pool/save/(?P<id_tournament>\d+)/(?P<id_pool>\d+)/(?P<id_match>\d+)$', 'save_match_changes'),
 )
