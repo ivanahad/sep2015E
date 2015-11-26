ec69b716e23858f66e0356f963e34c50116a25e8
Siceron <ludovicfastre@yahoo.fr>
2015-11-26 16:42:32 +0100

    git merge... yay :D

--- sep2015E/players/urls.py
+++ sep2015E/players/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('players.views',
     url(r'^register$', 'register'),
-<<<<<<< HEAD
-    url(r'^fregister', 'filled_registration'),
-=======
     url(r'^payement$', 'payement'),
     url(r'^fregister$', 'filled_registration'),
->>>>>>> origin/master
     url(r'^unregister$', 'unregister'),
     url(r'^unregister_confirm$', 'unregister_confirm'),
 )
