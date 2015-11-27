093be2c35d21316618a37c7b9eebb081ea9f8855
Siceron <ludovicfastre@yahoo.fr>
2015-11-27 22:24:37 +0100

    merge migration

---
+++ sep2015E/tournament/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tournament', '0010_auto_20151127_1712'),
+        ('tournament', '0011_auto_20151127_1805'),
+    ]
+
+    operations = [
+    ]
