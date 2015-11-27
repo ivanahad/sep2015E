4c89aea1de836d5bec2102bb2b840ac9a8732ff6
Arnaud Dethise <arnauddethise@gmail.com>
2015-11-27 15:20:11 +0100

    merge migrations

---
+++ sep2015E/tournament/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tournament', '0005_auto_20151127_1453'),
+        ('tournament', '0006_match_date'),
+    ]
+
+    operations = [
+    ]
