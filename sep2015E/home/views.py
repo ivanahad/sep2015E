d3439300ba0fb8817ebd7057375b70b026fdd7e9
Siceron <ludovicfastre@yahoo.fr>
2015-12-05 16:05:29 +0100

    pythondoc home

--- sep2015E/home/views.py
+++ sep2015E/home/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
  
 def index(request):
+	""" Method who render index.html """
     return render_to_response('home/index.html', context_instance=RequestContext(request))
 
 def sponsors(request):
+	""" Method who render sponsors.html """
     return render_to_response('home/sponsors.html', context_instance=RequestContext(request))
 
 def contact(request):
+	""" Method who render contact.html """
     return render_to_response('home/contact.html', context_instance=RequestContext(request))
