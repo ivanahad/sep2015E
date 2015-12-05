ad9c56a2abad5adfeddc9947a597911c874d5a2d
Geoffroy <geoffroy@Ubuntu-15.0>
2015-12-05 17:10:35 +0100

    correction bug commentaires

--- sep2015E/home/views.py
+++ sep2015E/home/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
  
+""" Method who render index.html """
 def index(request):
-	""" Method who render index.html """
     return render_to_response('home/index.html', context_instance=RequestContext(request))
 
+""" Method who render sponsors.html """
 def sponsors(request):
-	""" Method who render sponsors.html """
     return render_to_response('home/sponsors.html', context_instance=RequestContext(request))
 
+""" Method who render contact.html """
 def contact(request):
-	""" Method who render contact.html """
     return render_to_response('home/contact.html', context_instance=RequestContext(request))
