df8b9568b6a831c285464447b7d2f27e3edac205
Siceron <ludovicfastre@yahoo.fr>
2015-12-07 17:02:57 +0100

    Migrations

---
+++ sep2015E/courts/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courts', '0006_auto_20151206_1615'),
+        ('courts', '0006_auto_20151206_1631'),
+    ]
+
+    operations = [
+    ]
